605e9a5d469975497bc0230fae7bbeab6f767fb6
ST10540680 <ST10540680@myemeris.edu.za>
2026-05-20 14:18:42 +0200

    added a comment and declared variables

--- app/src/main/java/com/example/multiplierapp/MainActivity.kt
+++ app/src/main/java/com/example/multiplierapp/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.multiplierapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +14,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        // Declaring variables
+        val editNum1 = findViewById<EditText>(R.id.editNum1)
+        val editNum2 = findViewById<EditText>(R.id.editNum2)
+        val btnCalc = findViewById<Button>(R.id.btnCalc)
+        val txtResult = findViewById<TextView>(R.id.txtResult)
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
